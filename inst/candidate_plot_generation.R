e345269beb6d72c970a13bbd2b82cb7ed9c202b5
ajrominger <ajrominger@gmail.com>
2019-10-29 16:38:24 -0600

    got working

--- inst/candidate_plot_generation.R
+++ inst/candidate_plot_generation.R
@@ -1,6 +1,4 @@
-library(sp)
-library(raster)
-library(rgdal)
+library(CaatingaArthropods)
 options(stringsAsFactors = FALSE)
 
 # read in data
@@ -10,8 +8,8 @@ lcoverkey <- data.frame(code = c(14, 20, 30, 40, 50, 60, 110, 120, 130,
                                  150, 180, 200, 210), 
                         type = c('Cropland', 'Mosaic mostly cropland', 'Mosaic mostly veg',
                                  'Closed to open evergreen/semideciduous forest', 
-                                 'Closed deciduous forest', 'Open deciduous foredt', 
-                                 'Mosaic mostly foresdt/shrubland', 
+                                 'Closed deciduous forest', 'Open deciduous forest', 
+                                 'Mosaic mostly forest/shrubland', 
                                  'Mosaic mostly grassland', 'Closed to open shrubland', 
                                  'Sparse vegetation', 
                                  'Grassland or woody veg on regularely flooded soil',
@@ -29,7 +27,15 @@ forest@data <- forest@data[, 1:3]
 names(forest@data) <- c('UF', 'type', 'region')
 forest$region <- gsub('_', '_0', forest$region)
 
+# find cover types that we don't want
+good <- c(40, 50, 60)
+bad <- rasterToPolygons(lcover, function(x) !(x %in% good), dissolve = TRUE)
 
-
-
-
+# made candidate points
+candArea <- sampArea(region = forest[forest$region == 'region_01', ], edgeBuffer = 100, 
+                     roads = roads, roadBufferMin = 500, roadBufferMax = 2500, 
+                     exclude = bad, 
+                     utmZone = 24)
+plot(candArea)
+candPnts <- sampPoints(12, candArea, 1000, name = 'caa01', seed = 123)
+points(candPnts)
