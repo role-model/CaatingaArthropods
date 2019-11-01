1c3a444c835f9612a2c02e842b9190e8a859feaf
ajrominger <ajrominger@gmail.com>
2019-11-01 13:19:32 -0600

    added info on data

--- R/loadCaatingaSpatial.R
+++ R/loadCaatingaSpatial.R
@@ -7,7 +7,14 @@
 #' 
 #' @param message a nice thing to say to yourself
 #' 
-#' @return Creats the following objects
+#' @return Creats the following objects:
+#' \describe{
+#'   \item{\code{forest}}{SOSMA fragments as a \code{SpatialPolygonsDataFrame}}
+#'   \item{\code{lcover}}{SOSMA landcover classes as a \code{raster}}
+#'   \item{\code{lcoverke}}{key to SOSMA landcover as a \code{data.frame}}
+#'   \item{\code{regions}}{a \code{SpatialPolygonsDataFrame} that delineates the different regions}
+#'   \item{\code{roads}}{a \code{SpatialLinesDataFrame} of the roads}
+#' }
 #'
 #' @author Andy Rominger <ajrominger@@gmail.com>
 #' @export 
